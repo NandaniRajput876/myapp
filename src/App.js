7840c5986c5165ff37dad697caf7d90560eaf300
NandaniRajput876 <nandanirajput876@gmail.com>
2024-08-05 23:00:10 +0530

    https://github.com/NandaniRajput876/template.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css"
+function App1(){
+    return(
+        <div id="main">
+            <div id="main1">
+              <h2 style={{color:"rgb(53, 78, 215)"}}>&emsp;<i class="fa-brands fa-mixer"></i>&emsp;Zeuron.ai</h2> 
+              <h4 style={{color:"rgb(62, 60, 60)"}}>&emsp;<i style={{backgroundColor:"rgb(62, 60, 60)",color:"whitesmoke",padding:10,borderRadius:5}}class="fa-solid fa-microphone"></i>&emsp;Emergencey help</h4>
+              <h4>&emsp;<i class="fa-solid fa-table"></i>&emsp;Example 1</h4>
+              <h4 style={{color:"blue"}}>&emsp;<i class="fa-regular fa-folder"></i>&emsp;Onboard Patient</h4>
+              <h4>&emsp;<i class="fa-regular fa-clock"></i>&emsp;Example 1</h4>
+              <h4>&emsp;<i class="fa-regular fa-calendar-minus"></i>&emsp;Example 1</h4>
+              <h4>&emsp;<i class="fa-regular fa-clock"></i>&emsp;Example 1</h4>
+              <h4>&emsp;<i class="fa-solid fa-users"></i>&emsp;Example 1</h4>
+              <h4>&emsp;<i class="fa-solid fa-stethoscope"></i>&emsp;Example 1</h4>
+              <h4>&emsp;<i class="fa-regular fa-message"></i>&emsp;Example 1</h4>
+              <h4>&emsp;<i class="fa-solid fa-paper-plane"></i>&emsp;Example 1</h4>
+              <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+              <h4>&emsp;<i class="fa-solid fa-list-check"></i>&emsp;Settings</h4>
+            </div>
+            <div id="main2">
+               <div id="first"style={{padding:"30px",backgroundColor:"rgb(240, 240, 240)"}}>
+               <h2><i style={{fontSize:43}}class="fa-regular fa-circle-user"></i>&emsp;Jerome Bellingham&emsp;<span style={{color:"green",fontSize:10,border:"1px solid green",backgroundColor:"rgb(187, 225, 187)",padding:"5px",borderRadius:"5px"}}>MEMBER</span></h2>
+               <h5 style={{paddingLeft:"60px"}}><i class="fa-regular fa-user"></i>&emsp;Joined Since : 5 August 2024 </h5>
+               <i style={{border:"1px solid black",padding:10,marginLeft:60,borderRadius:5}}class="fa-regular fa-envelope"></i>&emsp;<i style={{border:"1px solid black",padding:10,borderRadius:5}}class="fa-solid fa-phone"></i>&emsp;<i style={{border:"1px solid black",padding:10,borderRadius:5}}class="fa-solid fa-mobile-screen-button"></i>&emsp;<i style={{border:"1px solid black",padding:10,borderRadius:5}}class="fa-solid fa-video"></i>&emsp;<i style={{border:"1px solid black",padding:10,borderRadius:5}}class="fa-solid fa-note-sticky"></i>
+               <br></br>
+               <br></br>
+               <i style={{marginLeft:65,fontSize:25,color:"blue"}}class="fa-solid fa-circle-dot"></i>&emsp;<i style={{fontSize:25,color:"rgb(244, 241, 241)",border:"1px solid black",borderRadius:"50%"}}class="fa-solid fa-circle-dot"></i>&emsp;<i style={{fontSize:25,color:"rgb(244, 241, 241)",border:"1px solid black",borderRadius:"50%"}}class="fa-solid fa-circle-dot"></i>&emsp;<i style={{fontSize:25,color:"rgb(244, 241, 241)",border:"1px solid black",borderRadius:"50%"}}class="fa-solid fa-circle-dot"></i>&emsp;<i style={{fontSize:25,color:"rgb(244, 241, 241)",border:"1px solid black",borderRadius:"50%"}}class="fa-solid fa-circle-dot"></i>&emsp;<i style={{fontSize:25,color:"rgb(244, 241, 241)",border:"1px solid black",borderRadius:"50%"}}class="fa-solid fa-circle-dot"></i>
+               <div id="firsttable">
+                <h3>Basic Information</h3>
+                <h4 style={{color:"gray"}}><i class="fa-solid fa-venus"></i>&emsp;Gender</h4>
+                <h4 style={{paddingLeft:30}}>Male</h4>
+                <h4 style={{color:"gray"}}><i class="fa-solid fa-cake-candles"></i>&emsp;Birthday</h4>
+                <h4 style={{paddingLeft:30}}>18 August 2002</h4>
+                <h4 style={{color:"gray"}}><i class="fa-solid fa-phone"></i>&emsp;Phone Number</h4>
+                <h4 style={{paddingLeft:30}}>+91 457898766554</h4>
+                <h4 style={{color:"gray"}}><i class="fa-regular fa-envelope"></i>&emsp;Email</h4>
+                <h4 style={{paddingLeft:30}}>hbdebcjdck248@gmail.com</h4>
+                <h4 style={{color:"gray"}}><i class="fa-solid fa-people-arrows"></i>&emsp;Sources</h4>
+                <h4 style={{paddingLeft:30}}><i style={{border:"1px solid black",padding:5,borderRadius:5,color:"rgb(171, 30, 145)"}}class="fa-brands fa-facebook-messenger"></i>&emsp;<i style={{border:"1px solid black",padding:5,borderRadius:5,color:"blue"}}class="fa-brands fa-google"></i>&emsp;<i style={{border:"1px solid black",padding:5,borderRadius:5,color:"rgb(58, 158, 231)"}}class="fa-brands fa-telegram"></i>&emsp;<i style={{border:"1px solid black",padding:5,borderRadius:5,color:"green"}}class="fa-brands fa-whatsapp"></i></h4> 
+               </div>
+               <br></br>
+               <div id="assurance1">
+               <h3 style={{color:"white"}}>Date of Assurance&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;...</h3>
+               <h2 style={{color:"white"}}><i style={{fontSize:35}}class="fa-solid fa-address-card"></i>&emsp;234-234-232-32</h2>
+               <h4 style={{color:"gray"}}>&emsp;Expiry &emsp;&emsp;&emsp;&emsp;&emsp;Status</h4>
+               <h4 style={{color:"white"}}>23/04/2025 &emsp;&emsp;&emsp;&emsp;Actived</h4>
+               </div> 
+               </div>
+               <div id="second"style={{padding:"30px",backgroundColor:"rgb(240, 240, 240)",paddingTop:100}}>
+                <h3 style={{backgroundColor:"white"}}>Assessment</h3>
+                <h3 style={{backgroundColor:"rgb(234, 235, 235)"}}>Training</h3>
+                <br></br><br></br><br></br>
+                <h4><i style={{color:"blue",fontSize:30}}class="fa-regular fa-folder-open"></i>&emsp;Standards&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<i style={{color:"green"}} class="fa-solid fa-pen-to-square"></i>&emsp;&emsp;&emsp;&emsp;<i style={{color:"red"}}class="fa-regular fa-trash-can"></i></h4>
+                <hr></hr>
+                <h4><i style={{color:"rgb(190, 157, 24)",fontSize:30}}class="fa-regular fa-folder-open"></i>&emsp;XX Protocol&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<i style={{color:"green"}}class="fa-solid fa-pen-to-square"></i>&emsp;&emsp;&emsp;&emsp;<i style={{color:"red"}}class="fa-regular fa-trash-can"></i></h4>
+                <hr></hr>
+                <h4><i style={{color:"rgb(190, 157, 24)",fontSize:30}}class="fa-regular fa-folder-open"></i>&emsp;Create New&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;<i style={{color:"green"}}class="fa-solid fa-pen-to-square"></i>&emsp;&emsp;&emsp;&emsp;<i style={{color:"red"}}class="fa-regular fa-trash-can"></i></h4>
+               </div>
+               <div id="third"style={{padding:"30px",backgroundColor:"rgb(240, 240, 240)"}}>
+               <h2>Case History Files&emsp;&emsp;&emsp;&emsp;...</h2>
+               <h4><i class="fa-regular fa-file-zipper"></i>&emsp;Agreement_Meditation.zip&emsp;<i class="fa-solid fa-cloud-arrow-down"></i></h4>
+               <h4><i class="fa-regular fa-file-pdf"></i>&emsp;Provision of Information.pdf&emsp;<i class="fa-solid fa-cloud-arrow-down"></i></h4>
+               <h4><i class="fa-regular fa-file-zipper"></i>&emsp;Agreement_Meditation.zip&emsp;<i class="fa-solid fa-cloud-arrow-down"></i></h4>
+               <h4><i class="fa-regular fa-file-pdf"></i>&emsp;Provision of Infromation.pdf&emsp;<i class="fa-solid fa-cloud-arrow-down"></i></h4>
+               </div>
+               <div id="fourth"style={{padding:"30px",backgroundColor:"rgb(240, 240, 240)"}}>
+              <h2>Appointment Schedule</h2>
+              <h2 id="fourth1">+</h2>
+               </div>
+                
+            </div>
+            <h2 id="ass"style={{color:"rgb(62, 60, 60)"}}>Assignment</h2>
+             
+        </div>
+        
+    )
 }
-
-export default App;
+export default App1;
